77c085df9b2006ba971176fa82e322ae45aa4c4f
cshooting <key874@qq.com>
2018-12-05 14:36:58 +0800

    singer

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -12,3 +12,33 @@ export function addClass(el, className) {
   newClass.push(className)
   el.className = newClass.join(' ')
 }
+
+let elementStyle = document.createElement("div").style;
+
+let vendor = (()=>{
+  let transformName = {
+    webkit: 'webkitTransform',
+    Moz: 'MozTransform',
+    O: 'OTransform',
+    ms: 'msTransform',
+    standard: 'transform'
+  }
+  for (let key in transformName){
+    if (elementStyle[transformName[key]]!= "undefined"){
+      return key;
+    }
+  }
+  return false;
+})()
+
+export function prefixStyle(style) {
+  if (vendor === false){
+    return false;
+  }
+
+  if (vendor === 'standard'){
+    return style;
+  }
+
+  return vendor + style.charAt(0).toUpperCase() + style.substr(1);
+}
--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -4,7 +4,14 @@
       <i class="icon-back"></i>
     </div>
     <h1 class="title" v-html="title"></h1>
-    <div class="bg-image" :style="bgStyle" ref="bgImage"></div>
+    <div class="bg-image" :style="bgStyle" ref="bgImage">
+      <div class="play-wrapper">
+        <div class="play" ref="playBtn" v-show="songs.length>0">
+          <i class="icon-play"></i>
+          <span class="text">随机播放</span>
+        </div>
+      </div>
+    </div>
     <div class="bg-layer" ref="layer"></div>
     <scroll @scroll="scroll" :probeType="probeType" :listenScroll="listenScroll" :data="songs" ref="list" class="list">
       <div class="song-list-wrapper">
@@ -17,8 +24,11 @@
 <script>
   import Scroll from "@/base/scroll/scroll"
   import SongList from "@/base/song-list/song-list"
+  import {prefixStyle} from "@/common/js/dom";
 
   const RESERVED_HEIGHT = 40
+  const transform = prefixStyle("transform")  //prefixStyle函数检测浏览器兼容性
+
   export default {
     name: "music-list",
     components: {SongList, Scroll},
@@ -58,7 +68,6 @@
     methods:{
       scroll(pos){
         this.scrollY = pos.y;
-        // console.log(this.scrollY)
       },
       back(){
         this.$router.back();
@@ -68,15 +77,24 @@
       scrollY(newVal) {
         let zIndex = 0;
         let translateY = Math.max(this.minTranslateY,newVal);
-        this.$refs.layer.style['transform'] = `translate3d(0,${translateY}px,0)`;
+        let scale = 1;
+        const precent = Math.abs(newVal/this.imageHeight);
+        if (newVal>0){
+          scale = 1+precent;
+          zIndex = 10;
+        }
+        this.$refs.layer.style[transform] = `translate3d(0,${translateY}px,0)`;
         if (newVal < this.minTranslateY){
           zIndex = 10;
           this.$refs.bgImage.style.paddingTop = 0;
           this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`;
+          this.$refs.playBtn.style.display = "none";
         } else {
           this.$refs.bgImage.style.paddingTop = "70%";
           this.$refs.bgImage.style.height = 0;
+          this.$refs.playBtn.style.display = "";
         }
+        this.$refs.bgImage.style[transform] = `scale(${scale})`
         this.$refs.bgImage.style.zIndex = zIndex;
       }
     }
@@ -121,6 +139,30 @@
       padding-top 70%
       transform-origin top
       background-size cover
+      .play-wrapper
+        position absolute
+        bottom 20px
+        z-index: 50
+        width 100%
+        .play
+          box-sizing border-box
+          width 135px
+          padding 7px 0
+          margin 0 auto
+          text-align center
+          border 1px solid $color-theme
+          color $color-theme
+          border-radius 100px
+          font-size 0
+          .icon-play
+            display inline-block
+            vertical-align middle
+            margin-right 6px
+            font-size $font-size-medium-x
+          .text
+            display inline-block
+            vertical-align middle
+            font-size $font-size-small
     .bg-layer
       position relative
       height 100%
